20d546da26641007d62dc008e6196e84396cf41c
Andrew Thompson <mnemonix.lstudios@gmail.com>
2023-01-05 17:20:39 +0000

    Did a little bit of view work

--- Source/Valspire.Shell/Program.cs
+++ Source/Valspire.Shell/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Valspire.Core.CharacterCreation.Modes;
 using Valspire.Func.Primitives.TextPrimitive;
+using Valspire.TextView.Views;
 using Valspire.TextView.Views.CharacterCreation;
 
 Text ReadLine()
@@ -14,5 +15,36 @@ Text ReadLine()
 	}
 }
 
-var view = new ChoosingCharacterNameView(new ChoosingCharacterNameMode());
-view.Start(t  => Console.Write(t.Value), ReadLine);
+View GetViewForMode(Mode m)
+{
+	return m switch
+	{
+		ChoosingCharacterNameMode choosingCharacterNameMode => () => new ChoosingCharacterNameView(choosingCharacterNameMode).Start(t => Console.Write(t.Value), ReadLine),
+		_ => CreateUnimplementedView(m)
+	};
+
+	View CreateUnimplementedView(Mode mode)
+	{
+		return () =>
+		{
+			Console.WriteLine(
+				$"There is no view for the current game mode. The current Game mode is {mode}");
+			Console.ReadKey(true);
+			Environment.Exit(-1);
+			return mode;
+		};
+	}
+}
+
+Mode mode = new ChoosingCharacterNameMode();
+
+while (true)
+{
+	var view = GetViewForMode(mode);
+	var oldMode = mode;
+	mode = view();
+	if (mode == oldMode)
+	{
+		break;
+	}
+}
